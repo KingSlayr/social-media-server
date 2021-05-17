ecb0bdc648a5fda09db74641752f07806ec0ce46
Ankit <ankitrastogi31200@gmail.com>
2021-05-17 07:40:10 +0530

    changes4

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoDb : 'mongodb+srv://classed:Woro8GyIK9Cak3St@socialmediaweb.59ahh.mongodb.net/merng?retryWrites=true&w=majority',
+    password : 'Woro8GyIK9Cak3St',
+    'SECRET_KEY':'Some Very Secret Key'
+}
